6a69ca0fd3767d8a207742d62d2ee8f00458146d
Evanflow89 <ivanbaragone@gmail.com>
2022-07-12 18:13:38 +0200

    Added Controller

--- app/Train.php
+++ app/Train.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Train;
 
 class Train extends Model
 {
-    //
+    public function index() 
+    {
+  $trains = Train::all()
+    }
 }
