7ee0c0da3af1c6ec125bfe348539469fdd407acf
Evanflow89 <ivanbaragone@gmail.com>
2022-07-12 18:41:31 +0200

    Bloccato

--- app/Train.php
+++ app/Train.php
@@ -4,11 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Train;
+use Carbon\Carbon;
 
 class Train extends Model
 {
     public function index() 
     {
-  $trains = Train::all()
+        $date = Carbon::now()->format('Y-m-d');
+  $trains = Train::whereDate('data_partenza', = $date)->get();
+dd($trains);
     }
 }
