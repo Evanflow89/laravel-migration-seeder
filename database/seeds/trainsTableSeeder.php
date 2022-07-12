cb353357222db28036961d3f924d60d17ad4a8e2
Evanflow89 <ivanbaragone@gmail.com>
2022-07-12 17:45:23 +0200

    bug prezzo biglietto

--- database/seeds/trainsTableSeeder.php
+++ database/seeds/trainsTableSeeder.php
@@ -14,7 +14,15 @@ class trainsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         $newTrain = new Train();
-        $newTrain->azienda = 
+        $newTrain->azienda = $faker->word();
+        $newTrain->stazione_di_partenza = $faker->city();
+        $newTrain->stazione_di_arrivo = $faker->city();
+        $newTrain->data_partenza = $faker->date();
+        $newTrain->orario_partenza = $faker->time();
+        $newTrain->orario_arrivo = $faker->time();
+        $newTrain->codice_treno = rand(1000,9999);
+        $newTrain->num_carrozze = rand(1,20);
+        $newTrain->prezzo_biglietto = rand(1,99);
         $newTrain->save();
     }
 }
