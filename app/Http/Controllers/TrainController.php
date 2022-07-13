baaef072a500dbe82ddfa94fb2e70f5a5c4a6bcb
Evanflow89 <ivanbaragone@gmail.com>
2022-07-13 09:16:47 +0200

    still bugs on comparison date

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Train;
+use Carbon\Carbon;
 
 class TrainController extends Controller
 {
-    //
+    public function index() 
+    {
+        $today = Carbon::today();
+  $trains = Train::table('trains')->whereDate('data_partenza', '$today')->get();
+dd($trains);
+    }
 }
